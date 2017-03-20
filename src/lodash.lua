13c6c0e72d2c72a93757949b1daf44ad3dfb66b6
Nicholas Farley <nicholasafarley@gmail.com>
2017-03-19 21:27:27 -0400

    Add documentation for _.assign

--- src/lodash.lua
+++ src/lodash.lua
@@ -1909,6 +1909,18 @@ end
 --- Object
 -- @section Object
 
+---
+-- Assigns own enumerable string keyed properties of source objects
+-- to the destination object. Source objects are applied from left
+-- to right. Subsequent sources overwrite property assignments of
+-- previous sources.
+--
+-- @usage _.print(_.assign({ a = 0 }, { a = 1 }, { b = 3 }))
+-- --> { a = 1, b = 3 }
+--
+-- @param object The destination object
+-- @param ... The source objects
+-- @return Returns object
 _.assign = function(object, ...)
   for index = 1, select('#', ...) do
     for k, v in pairs(select(index, ...)) do
